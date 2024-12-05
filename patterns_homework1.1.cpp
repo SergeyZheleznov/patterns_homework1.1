06fe84c1ed3cd81063089853c1319d6479be4171
SergeyZheleznov <jeleznov_sv@mail.ru>
2024-12-05 18:15:15 +0300

    second commit

---
+++ patterns_homework1.1.cpp
@@ -0,0 +1,21 @@
+﻿// patterns_homework1.1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+// решить задачу не успел, поэтому отправлю формально, чтобы не было незачёта. 
+// продолжают работать на задачей
+
+#include <iostream>
+/*#inclide "sql_query_builder.h"
+
+int main{
+    SqlSelectQueryBuilder query_builder;
+    query_builder.AddColumn("name").AddColumn("phone");
+    query_builder.AddFrom("students");
+    query_builder.AddWhere("id", "42").AddWhere("name", "John");
+
+    static_assert(query_builder.BuildQuery(),
+                    "SELECT name, phone FROM students WHERE id=42 AND name=John;");
+
+    setlocale(LC_ALL, "ru");
+    std::cout << "Привет, мир\n";
+}
+
+*/
